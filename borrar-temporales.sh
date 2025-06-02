54dceee2c3f6c86461ce23be1f7ef46ccb036e6e
profesom <dario.hermida@educa.madrid.org>
2025-06-02 13:13:32 +0200

    añadimos en Directorios, añadimos borrar pkz

--- borrar-temporales.sh
+++ borrar-temporales.sh
@@ -6,7 +6,7 @@ USUARIOS=("alumnosmrd" "alumnosmrv" "alumnodiurno" "alumnovespertino")
 # Los directorios /home/usuario/pruebas y /home/usuario/examen se usarán para las pruebas diarias.
 # El directorio: pt es para el packet tracer
 # El directorio pruebas y examen los uso para SOM y AOF
-DIRECTORIOS=("prueba" "examen" "Ejercicios" "ejercicios" "som" "Pruebas" "Imágenes" "Images" "Descargas" "Downloads" "Documentos" "Documents" "Music" "Música" "Trash" "Recientes" "pt" "Vídeos" "Videos" "pruebas" "examen" "Desktop" "Escritorio")
+DIRECTORIOS=("pepe" "mok" "Instituto" "somexamen" "instalaciones" "EJERCICIOS" "Som" "documentos" "home" "Pictures" "BasedeDatos" "instituto" "mi_directorio" "escritorios" "tutoriales" "som" "SOM" "directorio" "directorio1" "pruebas" "pruebas_SOM" "proyecto" "prueba-permisos" "pt/logs" "pt/saves" "prueba" "examen" "Ejercicios" "ejercicios" "som" "Pruebas" "Imágenes" "Images" "Descargas" "Downloads" "Documentos" "Documents" "Music" "Música" "Trash" "Recientes" "pt" "Vídeos" "Videos" "pruebas" "examen" "Desktop" "Escritorio")
 
 # Iterate over each usuario and each directorio
 for usuario in "${USUARIOS[@]}"; do
@@ -20,7 +20,7 @@ for usuario in "${USUARIOS[@]}"; do
             # Cambios para el directorio pt
             if [ "$directorio" == "pt" ]; then
                 echo "Borrando $DIR_ACTUAL ..."
-                find "$DIR_ACTUAL" -type f \( -name "*.pkt" -o -name "*.pka" \) -delete
+                find "$DIR_ACTUAL" -type f \( -name "*.pkz" -name "*.pkt" -o -name "*.pka" \) -delete
             fi
             echo "Listo."
         else
@@ -42,7 +42,7 @@ for usuario in "${USUARIOS[@]}"; do
     # actualmente quitamos: pdf, txt, doc, docx, xls, xlsx, ppt, pptx, 
     # actualmente quitamos: odt, odf, ods, .sh, .db, .bd, 
     # actualmente quitamos: pka, pkt, html
-    find "$HOME_DIR" -maxdepth 1 -type f \( -name "*.save" -o -name "*.pdf" -o -name "*.txt" -o -name "*.doc" -o -name "*.docx" -o -name "*.xls" -o -name "*.xlsx" -o -name "*.ppt" -o -name "*.pptx" -o -name "*.odt" -o -name "*.odf" -o -name "*.ods" -o -name "*.sh" -o -name "*.db" -o -name "*.pka" -o -name "*.html" -o -name "*.pkt" -o -name "*.bd" -o -name "*.js" -o -name "*.css" -o -name "*.png" \) -delete
+    find "$HOME_DIR" -maxdepth 4 -type f \( -name "*.tar" -name "*.pkz" -name "*.save" -o -name "*.pdf" -o -name "*.txt" -o -name "*.doc" -o -name "*.docx" -o -name "*.xls" -o -name "*.xlsx" -o -name "*.ppt" -o -name "*.pptx" -o -name "*.odt" -o -name "*.odf" -o -name "*.ods" -o -name "*.sh" -o -name "*.db" -o -name "*.pka" -o -name "*.html" -o -name "*.pkt" -o -name "*.bd" -o -name "*.js" -o -name "*.css" -o -name "*.png" -name ".odb" \) -delete
     echo "Listo los específicos para $HOME_DIR"
 
 done

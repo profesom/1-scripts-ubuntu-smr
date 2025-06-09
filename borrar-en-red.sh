d7d5239b10d6d4edff828386044112d88eae21b7
profesom <dario.hermida@educa.madrid.org>
2025-06-09 16:24:30 +0200

    Actualizamos la variable HOSTS, ya no hace falta. Se hace todo en el for con el rango de ip de los puestos. Faltaría pulir el número de pcs que se limpian bien.

--- borrar-en-red.sh
+++ borrar-en-red.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-HOSTS=("192.168.1.123" "192.168.1.156" "192.168.1.10" "192.168.1.164" "192.168.1.141" "192.168.1.120" "192.168.1.251" "192.168.1.124" "192.168.1.153" "192.168.1.148" "192.168.1.119" "192.168.1.128" "192.168.1.129" "192.168.1.125" "192.168.1.118" "192.168.1.149" "192.168.1.146" "192.168.1.154" "192.168.1.152" "192.168.1.131" "192.168.1.122")
 USERNAME="profesor"
 CMD1="sudo ls -lh /home/alumnosmrd/"
 CMD2="sudo ls -lh /home/alumnosmrv/"
@@ -8,11 +7,18 @@ LOCALSCRIPT="borrar-temporales.sh"
 REMOTEPATH="/tmp/borrar-temporales.sh"
 CMD3="/tmp/borrar-temporales.sh"
 
-for HOST in "${HOSTS[@]}"; do
-	echo "_________**para $HOST ahora**_____________"
-	echo "conectamos con $HOST"
-	scp "$LOCALSCRIPT" "$USERNAME@$HOST:$REMOTEPATH"
-	echo "hacemos $USERNAME@$HOST"	
-	ssh "$USERNAME@$HOST" "sudo $CMD3" 
-	echo "---------__finalizado $HOST__-------------"
+conexiones=0
+total_pcs_1smr=21
+
+for ((puesto=51; puesto<72; puesto++)); do
+	ip="192.168.1.$puesto"
+	echo "_________**para $ip ahora**_____________"
+	echo "conectamos con $ip"
+	scp "$LOCALSCRIPT" "$USERNAME@$ip:$REMOTEPATH"
+	echo "hacemos $USERNAME@$ip"
+	ssh "$USERNAME@$ip" "sudo $CMD3"
+	((conexiones++))
+	echo "---------__finalizado $ip__-------------"
 done
+
+# echo "Nos quedan $pendientes pcs por limpiar"
